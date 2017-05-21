e48601c46c31eeac5b15505a5c2882157562e397
Ben Kovacevich <bkovacevich@shutterstock.com>
2017-05-21 14:10:55 -0400

    buffer can overwrite data

--- tests/lib.companion_asset_editor.asset_buffer.js
+++ tests/lib.companion_asset_editor.asset_buffer.js
@@ -10,6 +10,7 @@ describe('AssetBuffer', function() {
     let buffer = new Buffer(new Array(8).fill(0x01));
 
     asset_buffer = new AssetBuffer(buffer);
+    asset_buffer.moveTo(0);
   });
 
   describe('#align', function() {
@@ -35,6 +36,68 @@ describe('AssetBuffer', function() {
       let buffer = asset_buffer.readBuffer(4);
 
       expect(buffer).to.be.an.instanceOf(AssetBuffer);
+    });
+  });
+
+  describe('#overwriteInt32LE', function() {
+    let new_number;
+    let offset;
+
+    function helperDumpBuffer(asset_buffer) {
+      asset_buffer.moveTo(0);
+
+      let buffer_data = [];
+      while(asset_buffer.readOffset < 8) {
+        let data = asset_buffer.readInt32LE();
+        buffer_data.push(data);
+      }
+
+      return buffer_data;
+    }
+
+    beforeEach(function() {
+      new_number = 0x0005;
+      offset     = 4;
+    });
+
+    it('replaces the data at the offset', function() {
+      asset_buffer.overwriteInt32LE(new_number, offset);
+
+      let buffer_data = helperDumpBuffer(asset_buffer);
+
+      expect(buffer_data).to.deep.equal([ 16843009, 5 ]);
+    });
+
+    it('updates the read Offset', function() {
+      asset_buffer.overwriteInt32LE(new_number, offset);
+
+      expect(asset_buffer.readOffset).to.equal(8);
+    });
+
+    it('does not alter the length of the buffer', function() {
+      asset_buffer.overwriteInt32LE(new_number, offset);
+
+      expect(asset_buffer.length).to.equal(8);
+    });
+
+    context('when there is no offset', function() {
+      beforeEach(function() {
+        offset = undefined;
+        asset_buffer.moveTo(4);
+      });
+
+      it('uses the current position', function() {
+        asset_buffer.overwriteInt32LE(new_number, offset);
+
+        asset_buffer.moveTo(0);
+        var buffer_data = [];
+        while(asset_buffer.readOffset < 8) {
+          let data = asset_buffer.readInt32LE();
+          buffer_data.push(data);
+        }
+
+        expect(buffer_data).to.deep.equal([ 16843009, 5 ]);
+      });
     });
   });
 });
